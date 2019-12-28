90225a21ec04a73fdd2fa419777f712a6a72bb36
kêdoská <kedoska@users.noreply.github.com>
2019-12-27 19:31:22 -0600

    prevent "/dist/lib" indentation (#8)
    * simply the final npm layout
    * consider internal and public package.json
    
    I haven't found any simple way to simplify the import of the module. What I know is that I like to import the functions directly from the module like this:
    
    `import { func } from "@module"`
    
    and not like:
    
    `import { func } from "@module/dist/lib"`

---
+++ publish.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+npm run format
+
+if output=$(git status --porcelain) && [ "$output" ]; then
+  >&2 echo "Uncommitted changes"
+  exit 1
+fi
+
+npm run test || exit 1
+npm run lint || exit 1
+
+npm run build
+
+DEST=./dist/lib
+cp ./README.md $DEST
+cp ./package-public.json $DEST/package.json
+cd $DEST
+echo entering `pwd`
+# npm publish
+
+rm -rf ./dist/
